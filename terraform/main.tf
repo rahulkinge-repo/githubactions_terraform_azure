84784b4a8b3cc171bc8fa5b64c173bea5e24dc3f
rahulkinge <rk#pad01>
2022-10-07 14:56:44 -0400

    updated tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
     admin_username = "ubuntu"
 
     ssh_key {
-      key_data = secrets.SSH_KEY
+      key_data = "AAAAB3NzaC1yc2EAAAADAQABAAACAQDJSUSvzhn3SQi+AZXJz1tWjQQ5VrOuvM1Qgw6m+bB/9F3QsheEhPVq5j4giYm5ekyxXgJ4D+zefBJq0GxfUCaZEqt5fYTb0AZCBJYIaUEsuO38fwTFm0ZdlNwwsfsOvy9GA76CMDOa58x9MjSs8DSzGCJ0wfYdFbVxDqfWQtfEUlST+aT7r+DfqPiUe9dR+osc66Z+q5Iop4loXx46gEhxvY3JXFfyrLtG6osZXPNLlIeFP6JmE5gajD9wMc4qN9pYTTpH4CCKL/S2CSsGXeUFmvSCF2D6ZtB+84PWdKKNcKBlbI7GKMCZYskCVGWQrVM/lpsDztEvjAiKKDr0rUsFeKjSZL0UKrloh9Oju6xT0gplL4zLQbf5hV6kniQf3gZpANjNywOOlcaj/N1dkIaQpD0cD/LtAWhKkZD/fcAifVJqz3RPvgOIm07UFwq4Fxe1HgBHXU8fIecKu2HSsvJPiDiW6K6VDCZBooG1D3xwBftgvLtoQs6ueq7N1ksCLZWQZr7CkumEeZe2th7aQ/mxCHDzlcc37YR/GSnylXczBTmftyQ2BpIOzqUBhvqevoJstK/pN49XORsgOq7mP9Ay8muTmZ5sMEgS6my4DXH6PhE0KFwhFF6POt7jVLdRv/MDMIMs/r9CeJ80I9HwAmq+cjbqgVYiw29DAs9WmYdTJQ=="
     }
   }
 
