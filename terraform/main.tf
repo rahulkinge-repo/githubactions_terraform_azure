ec8f1970521067539e8e55827d456eea6b35303d
rahulkinge <rk#pad01>
2022-10-07 13:59:18 -0400

    updated tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,11 +29,3 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "eastus2"
   resource_group_name = azurerm_resource_group.tamops.name
 }
- 
-# Create Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.tamops.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "192.168.0.0/24"
-}
